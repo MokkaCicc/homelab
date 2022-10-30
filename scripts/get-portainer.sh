84bcd0a6dacf653004b27577f61915abfb839b29
MokkaCicc <mokkacicc@bave.me>
2022-10-30 23:38:53 +0100

    Adding macvlan ip addressing

--- scripts/get-portainer.sh
+++ scripts/get-portainer.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 docker volume create portainer_data
+docker network create lan \
+	-d macvlan \
+	--subnet=192.168.0.0/24 \
+	--gateway=192.168.0.254 \
+	-o parent=enp3s0
 docker run -d \
 	--name portainer \
 	--restart=always \
+	--network=lan \
+	--ip=192.168.0.101 \
 	-p 9000:9000 \
 	-p 8000:8000 \
 	-v /var/run/docker.sock:/var/run/docker.sock \
