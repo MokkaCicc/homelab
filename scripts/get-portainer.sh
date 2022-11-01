4582c1f03a6475ad40c8ef8ce13059046c0e45d7
MokkaCicc <mokkacicc@bave.me>
2022-11-01 09:56:24 +0100

    Add fix mac addresses

--- scripts/get-portainer.sh
+++ scripts/get-portainer.sh
@@ -1,15 +1,27 @@
 #!/bin/sh
+echo -n "Enter this server lan network (eg: 192.168.0.0/24): "
+read subnet
+echo -n "Enter this server gateway (eg: 192.168.0.1): "
+read gateway
+echo -n "Enter this server lan interface name (eg: enp0s1): "
+read interface
+echo -n "Enter the portainer IP address (eg: 192.168.0.10): "
+read ip
+echo -n "Enter the portainer MAC address (eg: BE-E8-C1-64-57-30): "
+read mac
+
 docker volume create portainer_data
 docker network create lan \
 	-d macvlan \
-	--subnet=192.168.0.0/24 \
-	--gateway=192.168.0.254 \
-	-o parent=enp3s0
+	--subnet=$subnet \
+	--gateway=$gateway \
+	-o parent=$interface
 docker run -d \
 	--name portainer \
 	--restart=always \
 	--network=lan \
-	--ip=192.168.0.101 \
+	--ip=$ip \
+	--mac-address=$mac \
 	-p 9000:9000 \
 	-p 8000:8000 \
 	-v /var/run/docker.sock:/var/run/docker.sock \
