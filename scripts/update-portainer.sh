112ca10c427531fc14566d0119fc1a09fe4a2303
MokkaCicc <mokkacicc@bave.me>
2022-11-02 19:49:03 +0100

    remove implicit latest tag

--- scripts/update-portainer.sh
+++ scripts/update-portainer.sh
@@ -15,4 +15,4 @@ docker run -d \
 	-p 9000:9000 \
 	-p 8000:8000 \
 	-v /var/run/docker.sock:/var/run/docker.sock \
-	-v portainer_data:/data portainer/portainer-ce:latest
+	-v portainer_data:/data portainer/portainer-ce
