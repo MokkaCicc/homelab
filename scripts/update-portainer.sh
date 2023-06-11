bf8313ce8a6ecd0626526e3d4fc94c7dba05f6bd
MokkaCicc <mokkacicc@bave.me>
2023-06-11 12:25:41 +0200

    add docker pull to get latest image

--- scripts/update-portainer.sh
+++ scripts/update-portainer.sh
@@ -6,6 +6,7 @@ read mac
 
 docker stop portainer
 docker rm portainer
+docker pull portainer/portainer-ce
 docker run -d \
 	--name portainer \
 	--restart=always \
@@ -15,4 +16,5 @@ docker run -d \
 	-p 9000:9000 \
 	-p 8000:8000 \
 	-v /var/run/docker.sock:/var/run/docker.sock \
-	-v portainer_data:/data portainer/portainer-ce
+	-v portainer_data:/data \
+	portainer/portainer-ce
