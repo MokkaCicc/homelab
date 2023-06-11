daf9cecc17361dafcb899165580ac0c5512e8aee
MokkaCicc <mokkacicc@bave.me>
2023-06-11 12:25:56 +0200

    improve lisibility

--- scripts/get-portainer.sh
+++ scripts/get-portainer.sh
@@ -25,4 +25,5 @@ docker run -d \
 	-p 9000:9000 \
 	-p 8000:8000 \
 	-v /var/run/docker.sock:/var/run/docker.sock \
-	-v portainer_data:/data portainer/portainer-ce
+	-v portainer_data:/data \
+	portainer/portainer-ce
